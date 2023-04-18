77c6a9d62d77219ff0af73e00fa4094ea887836e
Dawid Zganiacz <dawidzganiacz@int.pl>
2023-04-18 17:25:26 +0200

    Zadanie dzień 4

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int age = 23;
+string name = "Dawid";
+bool male = true;
+
+
+string result = "Nazywam się_" + name + "_i mam_" + age + "_lata";
+string result1 = "Jestem mężczyzną po 20";
+string result2 = "Jestem mężczyzną w wieku 20-25 lat";
+string result3 = "Jestem kimś innym";
+if (male) ;
+{
+    if (age > 20)
+    {
+        Console.WriteLine(result1);
+    }
+    else if (age > 25) ;
+    {
+        Console.WriteLine(result2);
+        if (name == "Dawid") ;
+        {
+            Console.WriteLine(result);
+        }
+    }
+}
+if (!(male)) ;
+{
+    Console.WriteLine(result3);
+}
