77004480cccdb3f88918df69b17c1f7abb1eedb9
Dawid Zganiacz <dawidzganiacz@int.pl>
2023-04-18 20:22:14 +0200

    Poprawka kosmetyczna dzień 4

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,7 +2,6 @@
 string name = "Dawid";
 bool male = true;
 
-
 string result = "Nazywam się_" + name + "_i mam_" + age + "_lata";
 string result1 = "Jestem mężczyzną po 20";
 string result2 = "Jestem mężczyzną w wieku 20-25 lat";
@@ -10,19 +9,15 @@ string result3 = "Jestem kimś innym";
 if (male) ;
 {
     if (age > 20)
-    {
+    
         Console.WriteLine(result1);
-    }
+    
     else if (age > 25) ;
-    {
+    
         Console.WriteLine(result2);
         if (name == "Dawid") ;
-        {
+        
             Console.WriteLine(result);
-        }
-    }
-}
-if (!(male)) ;
-{
-    Console.WriteLine(result3);
+        
+    
 }
